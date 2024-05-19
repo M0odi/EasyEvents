128eb80fd34049caa4d00951e202ea9c2c8b6c04
Vova Pereslavtsev <vovapereslavtsev.com>
2024-05-19 21:20:41 +0300

    sad

--- src/main/java/com/eventeasy/EventEasy/config/SecurityConfiguration.java
+++ src/main/java/com/eventeasy/EventEasy/config/SecurityConfiguration.java
@@ -1,7 +1,6 @@
 package com.eventeasy.EventEasy.config;
 
 
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
